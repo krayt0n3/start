fae1593600b3b58a8b2c3c818709e45800c8af9b
r8yt <hendersonkevin64@gmail.com>
2019-06-24 18:28:08 -0500

    try again

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -45,26 +45,7 @@ export default class SignUp extends Component {
   }
 
 
-  componentDidMount() {
-    fetch("https://blog-bidprime.herokuapp.com/api/")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            title: result.title,
-            author: result.author,
-            image: result.image,
-            body: result.body
-          });
-        },
-        
-        (error) => {
-          this.setState({
-            error
-          });
-        }
-      );
-  }
+
 
 
   render() {
