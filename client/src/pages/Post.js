e77f26c5832e959282b4ced44b66dc2bee7bae7f
Krayt0n3 <hendersonkevin64@gmail.com>
2019-06-25 08:02:54 -0500

    test

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -33,10 +33,8 @@ export default class SignUp extends Component {
     
     fetch(url, {
       method: "POST", // or 'PUT'
-      body: JSON.stringify(data), // data can be `string` or {object}!
-      headers:{
-        "Content-Type": "application/json"
-      }
+      body: JSON.stringify(data) // data can be `string` or {object}!
+      
     }).then(res => res.json())
       .then(response => console.log("Success:", JSON.stringify(response)))
       .catch(error => console.error("Error:", error));
