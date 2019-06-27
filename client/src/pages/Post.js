e6ec6b8cf99909e7fde747dbe5736f1453678b92
Krayt0n3 <hendersonkevin64@gmail.com>
2019-06-27 09:43:01 -0500

    fixed class component

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -8,42 +8,73 @@ export default class SignUp extends Component {
       title: "",
       author: "",
       image: "",
-      body: ""
-
+      body: "",
+      toHome: false
     };
   }
 
-  onChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
+  onChange = (e) => {	
+    /*	
+      Because we named the inputs to match their	
+      corresponding values in state, it's	
+      super easy to update the state	
+    */	
+    this.setState({ [e.target.name]: e.target.value });	
   }
 
 
   onSubmit = (e) => {
     e.preventDefault();
     // get our form data out of state
-    const { title, author, image, body} = this.state.value;
+    const { title, author, image, body} = this.state;
 
     var url = "https://blog-bidprime.herokuapp.com/api/post/create";
-    
+    var data = {title, author, image, body};
     
     fetch(url, {
       method: "POST",
-      body: JSON.stringify({
-        title: title,
-        author: author,
-        image: image,
-        body: body
-      }),
+      body: JSON.stringify(data),
+      headers:{
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      }
       // data can be `string` or {object}!
-    }).then(res => console.log(res.json()));   
+    }).then(res => res.json())   
+      .then(response => console.log("Success:", JSON.stringify(response)))
+      .then(() => this.setState(() => ({	
+        toHome: true	
+      })))
+      .catch(error => console.error("Error:", error));
   }
 
+  componentDidMount() {
+    fetch("https://blog-bidprime.herokuapp.com/api/post/")	
+      .then(res => res.json())	
+      .then(	
+        (result) => {	
+          this.setState({	
+            title: result.title,	
+            author: result.author,	
+            image: result.image,	
+            body: result.body
+          });	
+        },	
 
+        (error) => {	
+          this.setState({	
+            error	
+          });	
+        }	
+      );	
+  }
+  
 
 
 
   render() {
     const { title, author, image, body, error} = this.state;
+    if (this.state.toProfile === true) {
+      return <Redirect to='/' />;}
     if (error) {
       return <div>Error: {error.message}</div>;
     } else {
